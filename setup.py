8a783567b7907ac8c5da6bfedd30ffcb207dcbf1
Eloy Felix <eloyfelix@gmail.com>
2019-01-18 12:47:48 +0000

    fix in memory ranges count

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: Chemistry'
