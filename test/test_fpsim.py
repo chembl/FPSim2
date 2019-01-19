00799521f2b9bfee117d686e4d30836eedb55f44
Eloy Felix <eloyfelix@gmail.com>
2019-01-19 21:32:01 +0000

    clean

--- test/test_fpsim.py
+++ test/test_fpsim.py
@@ -94,6 +94,6 @@ def test_delete_fps():
     delete_fps('test/10mols.h5', [11, 12, 13])
     fps = load_fps('test/10mols.h5')
     print(fps)
-    assert fps.shape[0] == 10
+    assert fps.fps.shape[0] == 10
     assert fps.fps[-1][-1] == 48
     assert fps.fps[0][-1] == 35
