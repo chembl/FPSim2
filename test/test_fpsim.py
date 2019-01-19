fc118ce52212b1cb20ed6e959c95965961127152
Eloy Felix <eloyfelix@gmail.com>
2019-01-19 22:58:03 +0000

    clean

--- test/test_fpsim.py
+++ test/test_fpsim.py
@@ -86,7 +86,6 @@ def test_append_fps():
 def test_sort_fp_file():
     sort_fp_file('test/10mols.h5')
     fps = load_fps('test/10mols.h5')
-    print(fps)
     assert fps.fps[-1][-1] == 48
     assert fps.fps[0][-1] == 2
 
