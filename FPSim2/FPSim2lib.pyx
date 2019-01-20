90e5f9ba7c9625159352cd7cc3e6501bfc4da672
Eloy Felix <eloyfelix@gmail.com>
2019-01-20 18:43:31 +0000

    first try

--- FPSim2/FPSim2lib.pyx
+++ FPSim2/FPSim2lib.pyx
@@ -48,7 +48,7 @@ cdef inline double _tanimoto_coeff(uint32_t int_count, uint32_t count_query, uin
 @cython.initializedcheck(False)
 cdef uint32_t i_popcount(uint64_t[:] query, uint64_t[:] other):
     cdef uint32_t int_count = 0
-    cdef uint8_t j
+    cdef uint8_t j = 0
     for j in range(0, query.shape[0], 4):
         # Use __builtin_popcountll for unsigned 64-bit integers (fps j+ 1 in other to skip the mol_id)
         # equivalent to https://github.com/WojciechMula/sse-popcount/blob/master/popcnt-builtin.cpp#L23
