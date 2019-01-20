5d3c46cb43a79f23992fdc53bdf176e2cce9c8e4
eloyfelix <eloyfelix@gmail.com>
2019-01-20 22:27:27 +0000

    return only id for sss
    * return only id for sss
    
    * uint32_t for ids

--- FPSim2/FPSim2.py
+++ FPSim2/FPSim2.py
@@ -1,6 +1,6 @@
 import multiprocessing as mp
 import concurrent.futures as cf
-from .FPSim2lib import similarity_search, _similarity_search, get_bounds_range
+from .FPSim2lib import run_search, _similarity_search, _substructure_search, get_bounds_range
 from .io import load_query, COEFFS
 import tables as tb
 import numpy as np
@@ -18,21 +18,23 @@ def on_disk_search(query, fp_filename, threshold=0.7, coeff='tanimoto', chunk_si
     :return: Numpy structured array with mol_id and coeff for each match.
     """
     with tb.open_file(fp_filename, mode='r') as fp_file:
-        fp_tpye = fp_file.root.config[0]
+        fp_type = fp_file.root.config[0]
         count_ranges = fp_file.root.config[3]
 
     query = load_query(query, fp_filename)
 
     if coeff == 'substructure':
-         # if substructure automatically set threshold to 1.0
+        empty_res = np.ndarray((0,), dtype='<u8')
         threshold = 1.0
-        if fp_tpye != 'RDKPatternFingerprint':
+        if fp_type != 'RDKPatternFingerprint':
             print('Warning: Running a substructure search with {} fingerprints. '
-                'Consider using RDKPatternFingerprint'.format(fp_tpye))
+                'Consider using RDKPatternFingerprint'.format(fp_type))
+    else:
+        empty_res = np.ndarray((0,), dtype=[('mol_id','u8'), ('coeff','f4')])
 
     fp_range = get_bounds_range(query, count_ranges, threshold, COEFFS[coeff])
     if not fp_range:
-        return np.ndarray((0,), dtype=[('mol_id','u8'), ('coeff','f4')])
+        return empty_res
     else:
         i_start = fp_range[0]
         i_end = fp_range[1]
@@ -40,7 +42,7 @@ def on_disk_search(query, fp_filename, threshold=0.7, coeff='tanimoto', chunk_si
     c_indexes = ((x, x + chunk_size) for x in range(i_start, i_end, chunk_size))
     results = []
     with cf.ProcessPoolExecutor(max_workers=n_processes) as ppe:
-        future_ss = {ppe.submit(similarity_search, query, fp_filename, indexes, threshold, COEFFS[coeff]): 
+        future_ss = {ppe.submit(run_search, query, fp_filename, indexes, threshold, COEFFS[coeff]): 
                         c_id for c_id, indexes in enumerate(c_indexes)}
         for future in cf.as_completed(future_ss):
             m = future_ss[future]
@@ -51,13 +53,13 @@ def on_disk_search(query, fp_filename, threshold=0.7, coeff='tanimoto', chunk_si
             except ValueError:
                 pass
             except Exception as e:
-                print('Chunk {} thread died: '.format(m), e)
+                print('Chunk {} process died: '.format(m), e)
     if results:
         np_res = np.concatenate(results)
         if coeff != 'substructure':
             np_res[::-1].sort(order='coeff')
     else:
-        np_res = np.ndarray((0,), dtype=[('mol_id','u8'), ('coeff','f4')])
+        np_res = empty_res
     return np_res
 
 
@@ -72,17 +74,24 @@ def search(query, fps, threshold=0.7, coeff='tanimoto', n_threads=1):
     :return: Numpy structured array with mol_id and coeff for each match.
     """
     if coeff == 'substructure':
+        empty_res = np.ndarray((0,), dtype='<u8')
         threshold = 1.0
+        search_func = _substructure_search
+    else:
+        empty_res = np.ndarray((0,), dtype=[('mol_id','u8'), ('coeff','f4')])
+        search_func = _similarity_search
+
     fp_range = get_bounds_range(query, fps.count_ranges, threshold, COEFFS[coeff])
     if not fp_range:
-        return np.ndarray((0,), dtype=[('mol_id','u8'), ('coeff','f4')])
+        return empty_res
     else:
         i_start = fp_range[0]
         i_end = fp_range[1]
 
     if n_threads == 1:
-        np_res = _similarity_search(query, fps.fps, threshold, COEFFS[coeff], i_start, i_end)
-        np_res[::-1].sort(order='coeff')
+        np_res = search_func(query, fps.fps, threshold, i_start, i_end)
+        if coeff != 'substructure':
+            np_res[::-1].sort(order='coeff')
         return np_res
     else:
         results = []
@@ -90,7 +99,7 @@ def search(query, fps, threshold=0.7, coeff='tanimoto', n_threads=1):
             chunk_size = int((i_end - i_start) / n_threads)
             c_indexes = [[x, x + chunk_size] for x in range(i_start, i_end, chunk_size)]
             c_indexes[-1][1] = i_end
-            future_ss = {tpe.submit(_similarity_search, query, fps.fps, threshold, COEFFS[coeff], chunk_idx[0], chunk_idx[1]): 
+            future_ss = {tpe.submit(search_func, query, fps.fps, threshold, chunk_idx[0], chunk_idx[1]): 
                             c_id for c_id, chunk_idx in enumerate(c_indexes)}
             for future in cf.as_completed(future_ss):
                 m = future_ss[future]
@@ -107,5 +116,5 @@ def search(query, fps, threshold=0.7, coeff='tanimoto', n_threads=1):
             if coeff != 'substructure':
                 np_res[::-1].sort(order='coeff')
         else:
-            np_res = np.ndarray((0,), dtype=[('mol_id','u8'), ('coeff','f4')])
+            np_res = empty_res
     return np_res
--- FPSim2/FPSim2lib.pyx
+++ FPSim2/FPSim2lib.pyx
@@ -14,7 +14,7 @@ cdef extern int __builtin_popcountll(unsigned long long) nogil
 
 
 cdef struct Result:
-    uint64_t mol_id
+    uint32_t mol_id
     float coeff
 
 
@@ -45,16 +45,85 @@ cdef inline double _tanimoto_coeff(uint32_t int_count, uint32_t count_query, uin
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.initializedcheck(False)
-cpdef _similarity_search(uint64_t[:] query, uint64_t[:, :] fps, double threshold, int coeff_func, int i_start, int i_end):
+cdef inline uint32_t _i_popcount(uint64_t[:] query, uint64_t[:] other) nogil:
+    cdef uint32_t int_count = 0
+    cdef uint32_t j
+    for j in range(0, query.shape[0], 4):
+        # Use __builtin_popcountll for unsigned 64-bit integers (fps j+ 1 in other to skip the mol_id)
+        # equivalent to https://github.com/WojciechMula/sse-popcount/blob/master/popcnt-builtin.cpp#L23
+        int_count += __builtin_popcountll(other[j + 1] & query[j])
+        int_count += __builtin_popcountll(other[j + 2] & query[j + 1])
+        int_count += __builtin_popcountll(other[j + 3] & query[j + 2])
+        int_count += __builtin_popcountll(other[j + 4] & query[j + 3])
+    return int_count
 
-    cdef int i
-    cdef int j
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.initializedcheck(False)
+cpdef _substructure_search(uint64_t[:] query, uint64_t[:, :] fps, double threshold, uint32_t i_start, uint32_t i_end):
+
+    cdef uint32_t i
+    cdef uint32_t j
     cdef uint32_t int_count = 0
     cdef uint32_t rel_co_count = 0
     cdef uint32_t query_count = 0
     cdef double coeff = 0.0
-    cdef int total_sims = 0
-    cdef int simres_length = 256
+    cdef uint32_t total_sims = 0
+    cdef uint32_t simres_length = 256
+
+    cdef uint32_t *results = <uint32_t *> malloc(simres_length * sizeof(uint32_t))
+
+    with nogil:
+        # precalc query popcount
+        for j in range(query.shape[0]):
+            query_count += __builtin_popcountll(query[j])
+
+        for i in range(i_start, i_end):
+            int_count += _i_popcount(query, fps[i])
+            for j in range(0, query.shape[0], 4):
+                rel_co_count +=  __builtin_popcountll(query[j] & ~fps[i, j + 1])
+                rel_co_count +=  __builtin_popcountll(query[j + 1] & ~fps[i, j + 2])
+                rel_co_count +=  __builtin_popcountll(query[j + 2] & ~fps[i, j + 3])
+                rel_co_count +=  __builtin_popcountll(query[j + 3] & ~fps[i, j + 4])
+
+            coeff = _substruct_coeff(rel_co_count, int_count)
+
+            if coeff == threshold:
+                results[total_sims] = fps[i][0]
+                total_sims += 1
+
+            if total_sims == simres_length:
+                simres_length *= 2
+                results = <uint32_t *> realloc(results, simres_length * sizeof(uint32_t))
+
+            # reset values for next fp
+            int_count = 0
+            rel_co_count = 0
+
+    # this is happening inside the GIL
+    cdef np.ndarray np_results = np.ndarray((total_sims,), dtype='<u8')
+    for i in range(total_sims):
+        np_results[i] = results[i]
+
+    # free manually allocated memory and return the results
+    with nogil:
+        free(results)
+    return np_results
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.initializedcheck(False)
+cpdef _similarity_search(uint64_t[:] query, uint64_t[:, :] fps, double threshold, uint32_t i_start, uint32_t i_end):
+
+    cdef uint32_t i
+    cdef uint32_t j
+    cdef uint32_t int_count = 0
+    cdef uint32_t query_count = 0
+    cdef double coeff = 0.0
+    cdef uint32_t total_sims = 0
+    cdef uint32_t simres_length = 256
 
     # allocate number * sizeof(Result) bytes of memory
     cdef Result *results = <Result *> malloc(simres_length * sizeof(Result))
@@ -65,26 +134,8 @@ cpdef _similarity_search(uint64_t[:] query, uint64_t[:, :] fps, double threshold
             query_count += __builtin_popcountll(query[j])
 
         for i in range(i_start, i_end):
-            for j in range(0, query.shape[0], 4):
-                # Use __builtin_popcountll for unsigned 64-bit integers (fps j+ 1 in fps to skip the mol_id)
-                # equivalent to https://github.com/WojciechMula/sse-popcount/blob/master/popcnt-builtin.cpp#L23
-                int_count += __builtin_popcountll(fps[i, j + 1] & query[j])
-                int_count += __builtin_popcountll(fps[i, j + 2] & query[j + 1])
-                int_count += __builtin_popcountll(fps[i, j + 3] & query[j + 2])
-                int_count += __builtin_popcountll(fps[i, j + 4] & query[j + 3])
-
-                if coeff_func == 2:
-                    rel_co_count +=  __builtin_popcountll(query[j] & ~fps[i, j + 1])
-                    rel_co_count +=  __builtin_popcountll(query[j + 1] & ~fps[i, j + 2])
-                    rel_co_count +=  __builtin_popcountll(query[j + 2] & ~fps[i, j + 3])
-                    rel_co_count +=  __builtin_popcountll(query[j + 3] & ~fps[i, j + 4])
-
-            # tanimoto
-            if coeff_func == 0:
-                coeff = _tanimoto_coeff(int_count, query_count, fps[i, query.shape[0] + 1])
-            # substruct (tversky a=1, b=0 eq)
-            elif coeff_func == 2:
-                coeff = _substruct_coeff(rel_co_count, int_count)
+            int_count += _i_popcount(query, fps[i])
+            coeff = _tanimoto_coeff(int_count, query_count, fps[i, query.shape[0] + 1])
 
             if coeff >= threshold:
                 results[total_sims].mol_id = fps[i][0]
@@ -152,11 +203,14 @@ cpdef get_bounds_range(query, ranges, threshold, coeff):
     return range_to_keep
 
 
-def similarity_search(query, fp_filename, chunk_indexes, threshold, coeff):
+def run_search(query, fp_filename, chunk_indexes, threshold, coeff):
     with tb.open_file(fp_filename, mode='r') as fp_file:
         fps = fp_file.root.fps[chunk_indexes[0]:chunk_indexes[1]]
     num_fields = len(fps[0])
     fps2 = fps.view('<u8')
     fps3 = fps2.reshape(int(fps2.size / num_fields), num_fields)
-    res = _similarity_search(query, fps3, threshold, coeff, 0, fps.shape[0])
+    if coeff == 2:
+        res = _substructure_search(query, fps3, threshold, 0, fps.shape[0])
+    else:
+        res = _similarity_search(query, fps3, threshold, 0, fps.shape[0])
     return res
