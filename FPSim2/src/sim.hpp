40c35391e4e6d7f2847f82ca3f8f81b9ebe9dc76
Eloy Felix <eloyfelix@gmail.com>
2019-06-06 13:57:34 +0100

    to pybind11

--- FPSim2/src/sim.hpp
+++ FPSim2/src/sim.hpp
@@ -17,14 +17,14 @@ float tanimoto_coeff(uint32_t int_count,
 
 uint32_t py_popcount(py::array_t<unsigned long long> query);
 
-py::array_t<uint32_t> _substructure_search(py::array_t<unsigned long long> db,
-                                           py::array_t<unsigned long long> query,
+py::array_t<uint32_t> _substructure_search(py::array_t<unsigned long long> query,
+                                           py::array_t<unsigned long long> db,
                                            float threshold,
                                            uint32_t i_start,
                                            uint32_t i_end);
 
-py::array_t<Result> _similarity_search(py::array_t<unsigned long long> db,
-                                       py::array_t<unsigned long long> query,
+py::array_t<Result> _similarity_search(py::array_t<unsigned long long> query,
+                                       py::array_t<unsigned long long> db,
                                        float threshold,
                                        uint32_t i_start,
                                        uint32_t i_end);
