de45de88e9e7bb40f7e5ecec47a1c99502900cae
Eloy Felix <eloyfelix@gmail.com>
2019-07-03 13:31:47 +0100

    some formatting

--- test/test_fpsim.py
+++ test/test_fpsim.py
@@ -26,8 +26,8 @@ class TestFPSim2(unittest.TestCase):
 
 
     def test_create_db_file(self):
-        create_db_file('test/10mols.smi', 'test/10mols.h5', fp_type, fp_params)
-        with tb.open_file('test/10mols.h5', mode='r') as fp_file:
+        create_db_file('./10mols.smi', './10mols.h5', fp_type, fp_params)
+        with tb.open_file('./10mols.h5', mode='r') as fp_file:
             config = fp_file.root.config
             assert config[0] == fp_type
             assert config[1]['radius'] == fp_params['radius']
@@ -36,9 +36,9 @@ class TestFPSim2(unittest.TestCase):
 
 
     def test_create_db_file_sdf(self):
-        create_db_file('test/10mols.sdf', 'test/10mols_sdf.h5',
+        create_db_file('./10mols.sdf', './10mols_sdf.h5',
                     fp_type, fp_params, mol_id_prop='mol_id')
-        with tb.open_file('test/10mols_sdf.h5', mode='r') as fp_file:
+        with tb.open_file('./10mols_sdf.h5', mode='r') as fp_file:
             config = fp_file.root.config
             assert config[0] == fp_type
             assert config[1]['radius'] == fp_params['radius']
@@ -48,8 +48,8 @@ class TestFPSim2(unittest.TestCase):
 
     def test_create_db_file_list(self):
         create_db_file([['CC', 1], ['CCC', 2], ['CCCC', 3]],
-                    'test/10mols_list.h5', fp_type, fp_params)
-        with tb.open_file('test/10mols_list.h5', mode='r') as fp_file:
+                    './10mols_list.h5', fp_type, fp_params)
+        with tb.open_file('./10mols_list.h5', mode='r') as fp_file:
             config = fp_file.root.config
             assert config[0] == fp_type
             assert config[1]['radius'] == fp_params['radius']
@@ -58,13 +58,13 @@ class TestFPSim2(unittest.TestCase):
 
 
     def test_create_db_file_sqla(self):
-        engine = create_engine('sqlite:///test/test.db')
+        engine = create_engine('sqlite:///./test.db')
         s = Session(engine)
         sql_query = "select mol_string, mol_id from structure"
         resprox = s.execute(sql_query)
 
-        create_db_file(resprox, 'test/10mols_sqla.h5', fp_type, fp_params)
-        with tb.open_file('test/10mols_sqla.h5', mode='r') as fp_file:
+        create_db_file(resprox, './10mols_sqla.h5', fp_type, fp_params)
+        with tb.open_file('./10mols_sqla.h5', mode='r') as fp_file:
             config = fp_file.root.config
             assert config[0] == fp_type
             assert config[1]['radius'] == fp_params['radius']
@@ -73,20 +73,20 @@ class TestFPSim2(unittest.TestCase):
 
 
     def test_load_fps(self):
-        fps = load_fps('test/10mols.h5')
+        fps = load_fps('./10mols.h5')
         assert fps.fps.shape[0] == 10
         assert fps.fps.shape[1] == 34
         assert fps.count_ranges != []
 
 
     def test_load_fps_sort(self):
-        fps = load_fps('test/10mols.h5')
-        fps2 = load_fps('test/10mols.h5', sort=True)
+        fps = load_fps('./10mols.h5')
+        fps2 = load_fps('./10mols.h5', sort=True)
         assert fps2.count_ranges == fps.count_ranges
 
 
     def test_search(self):
-        fpe = FPSim2Engine('test/10mols.h5')
+        fpe = FPSim2Engine('./10mols.h5')
         results = fpe.similarity(query_smi, 0.7, n_workers=1)
         assert results.shape[0] == 4
         assert list(results[0]) == [1, 1.0]
@@ -94,7 +94,7 @@ class TestFPSim2(unittest.TestCase):
 
     def test_validate_against_rdkit(self):
 
-        with open('test/10mols.smi') as f:
+        with open('./10mols.smi') as f:
             smiles = f.readlines()
         fps = [Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smi),
                                                                 radius=2, nBits=2048) for smi in smiles]
@@ -103,14 +103,14 @@ class TestFPSim2(unittest.TestCase):
         rdresults = sorted([DataStructs.TanimotoSimilarity(query, fp)
                             for fp in fps], reverse=True)
 
-        fpe = FPSim2Engine('test/10mols.h5')
+        fpe = FPSim2Engine('./10mols.h5')
         results = fpe.similarity(query_smi, 0.0, n_workers=1)['coeff']
         for rds, fpss in zip(rdresults, results):
             assert True == math.isclose(rds, fpss, rel_tol=1e-7)
 
 
     def test_on_disk_search(self):
-        fpe = FPSim2Engine('test/10mols.h5', in_memory_fps=False)
+        fpe = FPSim2Engine('./10mols.h5', in_memory_fps=False)
         results = fpe.on_disk_similarity(
             query_smi, 0.7, chunk_size=100000, n_workers=2)
         assert results.shape[0] == 4
@@ -124,22 +124,22 @@ class TestFPSim2(unittest.TestCase):
 
 
     def test_append_fps(self):
-        append_fps('test/10mols.h5', [['CC', 11], ['CCC', 12], ['CCCC', 13]])
-        fps = load_fps('test/10mols.h5')
+        append_fps('./10mols.h5', [['CC', 11], ['CCC', 12], ['CCCC', 13]])
+        fps = load_fps('./10mols.h5')
         assert fps.fps.shape[0] == 13
 
 
     def test_sort_db_file(self):
-        sort_db_file('test/10mols.h5')
-        fps = load_fps('test/10mols.h5')
+        sort_db_file('./10mols.h5')
+        fps = load_fps('./10mols.h5')
         assert fps.fps[-1][-1] == 48
         assert fps.fps[0][-1] == 2
 
 
     def test_delete_fps(self):
-        delete_fps('test/10mols.h5', [11, 12, 13])
-        sort_db_file('test/10mols.h5')
-        fps = load_fps('test/10mols.h5')
+        delete_fps('./10mols.h5', [11, 12, 13])
+        sort_db_file('./10mols.h5')
+        fps = load_fps('./10mols.h5')
         assert fps.fps.shape[0] == 10
         assert fps.fps[-1][-1] == 48
         assert fps.fps[0][-1] == 35
