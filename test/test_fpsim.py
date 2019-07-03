7242f5188306de0895be7f12ec10ee1e304a0119
Eloy Felix <eloyfelix@gmail.com>
2019-07-03 14:18:07 +0100

    some formatting

--- test/test_fpsim.py
+++ test/test_fpsim.py
@@ -26,7 +26,7 @@ fp_params = {"radius": 2, "nBits": 2048}
 
 
 class TestFPSim2(unittest.TestCase):
-    def test_rdmol_top_efp(self):
+    def test_a_rdmol_top_efp(self):
         rdmol = Chem.MolFromSmiles("CCC")
         ok = [
             0,
@@ -64,7 +64,7 @@ class TestFPSim2(unittest.TestCase):
         ]
         assert rdmol_to_efp(rdmol, fp_type, fp_params) == ok
 
-    def test_create_db_file(self):
+    def test_b_create_db_file(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.smi')
         out_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         create_db_file(in_file, out_file, fp_type, fp_params)
@@ -75,7 +75,7 @@ class TestFPSim2(unittest.TestCase):
             assert config[1]["nBits"] == fp_params["nBits"]
             assert fp_file.root.fps.shape[0] == 10
 
-    def test_create_db_file_sdf(self):
+    def test_c_create_db_file_sdf(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.sdf')
         out_file = os.path.join(TESTS_DIR, 'data/10mols_sdf.h5')
         create_db_file(
@@ -88,7 +88,7 @@ class TestFPSim2(unittest.TestCase):
             assert config[1]["nBits"] == fp_params["nBits"]
             assert fp_file.root.fps.shape[0] == 10
 
-    def test_create_db_file_list(self):
+    def test_d_create_db_file_list(self):
         out_file = os.path.join(TESTS_DIR, 'data/10mols_list.h5')
         create_db_file(
             [["CC", 1], ["CCC", 2], ["CCCC", 3]], out_file, fp_type, fp_params
@@ -100,7 +100,7 @@ class TestFPSim2(unittest.TestCase):
             assert config[1]["nBits"] == fp_params["nBits"]
             assert fp_file.root.fps.shape[0] == 3
 
-    def test_create_db_file_sqla(self):
+    def test_e_create_db_file_sqla(self):
         in_file = os.path.join(TESTS_DIR, 'data/test.db')
         out_file = os.path.join(TESTS_DIR, 'data/10mols_sqla.h5')
         engine = create_engine("sqlite:///{}".format(in_file))
@@ -116,29 +116,30 @@ class TestFPSim2(unittest.TestCase):
             assert config[1]["nBits"] == fp_params["nBits"]
             assert fp_file.root.fps.shape[0] == 10
 
-    def test_load_fps(self):
+    def test_f_load_fps(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         fps = load_fps(in_file)
         assert fps.fps.shape[0] == 10
         assert fps.fps.shape[1] == 34
         assert fps.count_ranges != []
 
-    def test_load_fps_sort(self):
+    def test_g_load_fps_sort(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         fps = load_fps(in_file)
         fps2 = load_fps(in_file, sort=True)
         assert fps2.count_ranges == fps.count_ranges
 
-    def test_search(self):
+    def test_h_search(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         fpe = FPSim2Engine(in_file)
         results = fpe.similarity(query_smi, 0.7, n_workers=1)
         assert results.shape[0] == 4
         assert list(results[0]) == [1, 1.0]
 
-    def test_validate_against_rdkit(self):
-        in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
-        with open(in_file) as f:
+    def test_i_validate_against_rdkit(self):
+        in_file_smi = os.path.join(TESTS_DIR, 'data/10mols.smi')
+        in_file_h5 = os.path.join(TESTS_DIR, 'data/10mols.h5')
+        with open(in_file_smi) as f:
             smiles = f.readlines()
         fps = [
             Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(
@@ -153,19 +154,19 @@ class TestFPSim2(unittest.TestCase):
             [DataStructs.TanimotoSimilarity(query, fp) for fp in fps], reverse=True
         )
 
-        fpe = FPSim2Engine(in_file)
+        fpe = FPSim2Engine(in_file_h5)
         results = fpe.similarity(query_smi, 0.0, n_workers=1)["coeff"]
         for rds, fpss in zip(rdresults, results):
             assert True == math.isclose(rds, fpss, rel_tol=1e-7)
 
-    def test_on_disk_search(self):
+    def test_j_on_disk_search(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         fpe = FPSim2Engine(in_file, in_memory_fps=False)
         results = fpe.on_disk_similarity(query_smi, 0.7, chunk_size=100000, n_workers=2)
         assert results.shape[0] == 4
         assert list(results[0]) == [1, 1.0]
 
-    def test_py_popcount(self):
+    def test_k_py_popcount(self):
         res = py_popcount(
             np.array(
                 [
@@ -207,20 +208,20 @@ class TestFPSim2(unittest.TestCase):
         )
         assert res == 4
 
-    def test_append_fps(self):
+    def test_l_append_fps(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         append_fps(in_file, [["CC", 11], ["CCC", 12], ["CCCC", 13]])
         fps = load_fps(in_file)
         assert fps.fps.shape[0] == 13
 
-    def test_sort_db_file(self):
+    def test_m_sort_db_file(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         sort_db_file(in_file)
         fps = load_fps(in_file)
         assert fps.fps[-1][-1] == 48
-        assert fps.fps[0][-1] == 2
+        assert fps.fps[0][-1] == 2  
 
-    def test_delete_fps(self):
+    def test_n_delete_fps(self):
         in_file = os.path.join(TESTS_DIR, 'data/10mols.h5')
         delete_fps(in_file, [11, 12, 13])
         sort_db_file(in_file)
