96e233f2c0cfc1053ba93d520b4de0556f63b483
eloyfelix <eloyfelix@gmail.com>
2020-07-04 19:50:36 +0100

    clang formatting

--- FPSim2/src/sim.hpp
+++ FPSim2/src/sim.hpp
@@ -1,26 +1,21 @@
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-struct Result
-{
-    uint32_t idx;
-    uint32_t mol_id;
-    float coeff;
+struct Result {
+  uint32_t idx;
+  uint32_t mol_id;
+  float coeff;
 };
 
 __inline float substruct_coeff(uint64_t rel_co_count, uint64_t int_count);
 
-__inline float tanimoto_coeff(uint64_t int_count,
-                              uint32_t qcount,
+__inline float tanimoto_coeff(uint64_t int_count, uint32_t qcount,
                               uint32_t ocount);
 
-__inline float tversky_coeff(uint64_t int_count,
-                             uint64_t rel_co_count,
-                             uint64_t rel_co_count2,
-                             float a,
-                             float b);
+__inline float tversky_coeff(uint64_t int_count, uint64_t rel_co_count,
+                             uint64_t rel_co_count2, float a, float b);
 
 uint64_t py_popcount(py::array_t<uint64_t> pyquery);
 
@@ -28,18 +23,12 @@ void sort_results(py::array_t<Result> pyres);
 
 py::array_t<uint32_t> _substructure_search(py::array_t<uint64_t> pyquery,
                                            py::array_t<uint64_t> pydb,
-                                           float threshold,
-                                           float a,
-                                           float b,
-                                           uint8_t sim_type,
-                                           uint32_t i_start,
+                                           float threshold, float a, float b,
+                                           uint8_t sim_type, uint32_t i_start,
                                            uint32_t i_end);
 
 py::array_t<Result> _similarity_search(py::array_t<uint64_t> pyquery,
                                        py::array_t<uint64_t> pydb,
-                                       float threshold,
-                                       float a,
-                                       float b,
-                                       uint8_t sim_type,
-                                       uint32_t i_start,
+                                       float threshold, float a, float b,
+                                       uint8_t sim_type, uint32_t i_start,
                                        uint32_t i_end);
--- FPSim2/src/wraps.cpp
+++ FPSim2/src/wraps.cpp
@@ -1,15 +1,14 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
 #include "sim.hpp"
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(FPSim2lib, m)
-{
-    m.doc() = R"pbdoc(
+PYBIND11_MODULE(FPSim2lib, m) {
+  m.doc() = R"pbdoc(
         FPSim2lib
         ---------
-        .. currentmodule:: fpsim2
+        .. currentmodule:: FPSim2
         .. autosummary::
            :toctree: _generate
            py_popcount
@@ -18,27 +17,31 @@ PYBIND11_MODULE(FPSim2lib, m)
            _similarity_search
     )pbdoc";
 
-    PYBIND11_NUMPY_DTYPE(Result, idx, mol_id, coeff);
-    
-    m.def("py_popcount", &py_popcount, py::call_guard<py::gil_scoped_release>(), R"pbdoc(
+  PYBIND11_NUMPY_DTYPE(Result, idx, mol_id, coeff);
+
+  m.def("py_popcount", &py_popcount, py::call_guard<py::gil_scoped_release>(),
+        R"pbdoc(
         Calc popcount
         
         Calcs the popcount of a NumPy int array.
     )pbdoc");
 
-    m.def("sort_results", &sort_results, py::call_guard<py::gil_scoped_release>(), R"pbdoc(
+  m.def("sort_results", &sort_results, py::call_guard<py::gil_scoped_release>(),
+        R"pbdoc(
         Sort results
         
-        Sort, inplance, the results Numpy array.
+        Sort, inplace, the results Numpy array.
     )pbdoc");
 
-    m.def("_substructure_search", &_substructure_search, py::call_guard<py::gil_scoped_release>(), R"pbdoc(
+  m.def("_substructure_search", &_substructure_search,
+        py::call_guard<py::gil_scoped_release>(), R"pbdoc(
         Substructure search
 
         Runs a substructure search.
     )pbdoc");
 
-    m.def("_similarity_search", &_similarity_search, py::call_guard<py::gil_scoped_release>(), R"pbdoc(
+  m.def("_similarity_search", &_similarity_search,
+        py::call_guard<py::gil_scoped_release>(), R"pbdoc(
         Similarity search
 
         Runs a similarity search.
