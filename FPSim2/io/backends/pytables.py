673bad1425d0519fae3aef8c449fa77cfa5c6b2c
eloyfelix <eloyfelix@gmail.com>
2020-08-21 00:15:20 +0100

    fixed issue when generating DBs multiples of 10k, thanks adalke
    * libpopcnt update
    
    * change new_mols for fps in append_fps

--- FPSim2/io/backends/pytables.py
+++ FPSim2/io/backends/pytables.py
@@ -102,7 +102,8 @@ def create_db_file(
                 fps_table.append(fps)
                 fps = []
         # append last batch < 10k
-        fps_table.append(fps)
+        if fps:
+            fps_table.append(fps)
 
         # create index so table can be sorted
         fps_table.cols.popcnt.create_index(kind="full")
@@ -274,7 +275,7 @@ class PyTablesStorageBackend(BaseStorageBackend):
         fp_type, fp_params, _ = self.read_parameters()
         with tb.open_file(self.fp_filename, mode="a") as fp_file:
             fps_table = fp_file.root.fps
-            new_mols = []
+            fps = []
             for mol_id, rdmol in supplier(mols_source, False, mol_id_prop=mol_id_prop):
                 if not rdmol:
                     continue
@@ -282,9 +283,10 @@ class PyTablesStorageBackend(BaseStorageBackend):
                 popcnt = py_popcount(np.array(efp, dtype=np.uint64))
                 efp.insert(0, mol_id)
                 efp.append(popcnt)
-                new_mols.append(tuple(efp))
-                if len(new_mols) == BATCH_WRITE_SIZE:
-                    # append last batch < 10k
-                    fps_table.append(new_mols)
-                    new_mols = []
-            fps_table.append(new_mols)
+                fps.append(tuple(efp))
+                if len(fps) == BATCH_WRITE_SIZE:
+                    fps_table.append(fps)
+                    fps = []
+            # append last batch < 10k
+            if fps:
+                fps_table.append(fps)
