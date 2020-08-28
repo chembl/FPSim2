33450a40d19721a56865a857ddfaa97757d638f0
Eloy Felix <eloyfelix@gmail.com>
2020-08-28 23:07:24 +0100

    format headers

--- FPSim2/src/sim.hpp
+++ FPSim2/src/sim.hpp
@@ -9,26 +9,26 @@ struct Result {
     float coeff;
 };
 
-__inline float SubstructCoeff(uint64_t rel_co_count, uint64_t int_count);
+__inline float SubstructCoeff(uint64_t rel_co_popcnt, uint64_t common_popcnt);
 
-__inline float TanimotoCoeff(uint64_t int_count, uint32_t qcount,
-    uint32_t ocount);
+__inline float TanimotoCoeff(uint64_t common_popcnt, uint32_t qcount,
+                             uint32_t ocount);
 
-__inline float TverskyCoeff(uint64_t int_count, uint64_t rel_co_count,
-    uint64_t rel_co_count2, float a, float b);
+__inline float TverskyCoeff(uint64_t common_popcnt, uint64_t rel_co_popcnt,
+                            uint64_t rel_co_popcnt2, float a, float b);
 
 uint64_t PyPopcount(py::array_t<uint64_t> pyquery);
 
 void SortResults(py::array_t<Result> pyres);
 
-py::array_t<uint32_t> SubstructureScreenout(py::array_t<uint64_t> pyquery,
-    py::array_t<uint64_t> pydb,
-    float threshold, float a, float b,
-    uint8_t sim_type, uint32_t i_start,
-    uint32_t i_end);
+py::array_t<uint32_t> SubstructureScreenout(py::array_t<uint64_t> py_query,
+                                            py::array_t<uint64_t> py_db,
+                                            float threshold, float a, float b,
+                                            uint8_t sim_type, uint32_t i_start,
+                                            uint32_t i_end);
 
-py::array_t<Result> SimilaritySearch(py::array_t<uint64_t> pyquery,
-    py::array_t<uint64_t> pydb,
-    float threshold, float a, float b,
-    uint8_t sim_type, uint32_t i_start,
-    uint32_t i_end);
+py::array_t<Result> SimilaritySearch(py::array_t<uint64_t> py_query,
+                                     py::array_t<uint64_t> py_db,
+                                     float threshold, float a, float b,
+                                     uint8_t sim_type, uint32_t i_start,
+                                     uint32_t i_end);
