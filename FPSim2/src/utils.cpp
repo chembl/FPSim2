14c4493c07302ef1d726b59150eb7b49b58d68a2
Eloy Félix <eloyfelix@gmail.com>
2022-04-14 18:51:54 +0200

    fix compile Python3.10/Windows issue (#74)

--- FPSim2/src/utils.cpp
+++ FPSim2/src/utils.cpp
@@ -4,6 +4,11 @@
 #include <pybind11/pybind11.h>
 #include <string>
 
+#if defined(_MSC_VER)
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif
+
 namespace py = pybind11;
 
 namespace utils {
--- setup.py
+++ setup.py
@@ -137,10 +137,10 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Chemistry",
     ],
 )
