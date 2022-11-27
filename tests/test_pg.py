8b85f613ebed8d5c323fab7da99969237be2b5ac
Eloy Felix <eloyfelix@gmail.com>
2022-11-27 01:31:21 +0000

    test postgresql

---
+++ tests/test_pg.py
@@ -0,0 +1,76 @@
+from FPSim2.io.backends.sqla import create_db_table
+from FPSim2 import FPSim2Engine
+import tables as tb
+import pytest
+import sys
+import os
+
+try:
+    import psycopg2
+except:
+    pass
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+FP_TYPE = "Morgan"
+FP_PARAMS = {"radius": 2, "nBits": 2048}
+PG_URL = "postgresql://postgres:postgres@postgres:5432/postgres"
+
+
+with tb.open_file(os.path.join(TESTS_DIR, "data/test.h5"), mode="r") as fp_file:
+    fps = fp_file.root.fps[:]
+    num_fields = len(fps[0])
+    fps = fps.view("<u8")
+    fps = fps.reshape(int(fps.size / num_fields), num_fields)
+    popcnt_bins = fp_file.root.config[3]
+
+
+@pytest.mark.skipif("psycopg2" not in sys.modules, reason="requires psycopg2")
+def test_create_db_file_smi():
+    in_file = os.path.join(TESTS_DIR, "data/10mols.smi")
+    create_db_table(in_file, PG_URL, "fpsim2_fp_smi", FP_TYPE, FP_PARAMS)
+    fpe = FPSim2Engine(
+        conn_url=PG_URL, table_name="fpsim2_fp_smi", storage_backend="sqla"
+    )
+    fp_type, fp_params, _ = fpe.storage.read_parameters()
+    assert fp_type == FP_TYPE
+    assert fp_params["radius"] == FP_PARAMS["radius"]
+    assert fp_params["nBits"] == FP_PARAMS["nBits"]
+    assert fpe.fps.shape[0] == 10
+    assert fpe.popcnt_bins == popcnt_bins
+    assert fpe.fps.all() == fps.all()
+
+
+@pytest.mark.skipif("psycopg2" not in sys.modules, reason="requires psycopg2")
+def test_create_db_file_sdf():
+    in_file = os.path.join(TESTS_DIR, "data/10mols.sdf")
+    create_db_table(
+        in_file, PG_URL, "fpsim2_fp_sdf", FP_TYPE, FP_PARAMS, mol_id_prop="mol_id"
+    )
+    fpe = FPSim2Engine(
+        conn_url=PG_URL, table_name="fpsim2_fp_sdf", storage_backend="sqla"
+    )
+    fp_type, fp_params, _ = fpe.storage.read_parameters()
+    assert fp_type == FP_TYPE
+    assert fp_params["radius"] == FP_PARAMS["radius"]
+    assert fp_params["nBits"] == FP_PARAMS["nBits"]
+    assert fpe.fps.shape[0] == 10
+    assert fpe.popcnt_bins == popcnt_bins
+    assert fpe.fps.all() == fps.all()
+
+@pytest.mark.skipif("psycopg2" not in sys.modules, reason="requires psycopg2")
+def test_create_db_file_list():
+    create_db_table(
+        [["CC", 1], ["CCC", 2], ["CCCC", 3]],
+        PG_URL,
+        "fpsim2_fp_list",
+        FP_TYPE,
+        FP_PARAMS,
+    )
+    fpe = FPSim2Engine(
+        conn_url=PG_URL, table_name="fpsim2_fp_list", storage_backend="sqla"
+    )
+    fp_type, fp_params, _ = fpe.storage.read_parameters()
+    assert fp_type == FP_TYPE
+    assert fp_params["radius"] == FP_PARAMS["radius"]
+    assert fp_params["nBits"] == FP_PARAMS["nBits"]
+    assert fpe.fps.shape[0] == 3
