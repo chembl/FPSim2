6109fe7fba6ea6f4712b0558bc15cb2289531cba
Eloy Felix <eloyfelix@gmail.com>
2024-10-24 09:36:42 +0100

    more efficient BitStrToIntList

--- FPSim2/io/chem.py
+++ FPSim2/io/chem.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Iterable as IterableType, Dict, List, Tuple, Union
+from typing import Any, Callable, Iterable as IterableType, Dict, Tuple, Union
 from FPSim2.FPSim2lib.utils import BitStrToIntList, PyPopcount
 from collections.abc import Iterable
 from rdkit.Chem import rdMolDescriptors
