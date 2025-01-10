3e253c843300d70d8bba32b459248802b022f2a8
Eloy Felix <eloyfelix@gmail.com>
2025-01-10 16:52:41 +0000

    FPSim2CudaEngine cleanup

--- FPSim2/FPSim2Cuda.py
+++ FPSim2/FPSim2Cuda.py
@@ -1,12 +1,11 @@
 from .io.chem import get_bounds_range
-from .FPSim2lib.utils import SortResults
 from .base import BaseEngine
 import numpy as np
 import cupy as cp
 
 
 class FPSim2CudaEngine(BaseEngine):
-    """FPSim2 class to run fast CPU searches.
+    """FPSim2 class to run fast GPU Tanimoto searches.
 
     Parameters
     ----------
@@ -17,27 +16,7 @@ class FPSim2CudaEngine(BaseEngine):
         Wheter if the FPs should be sorted after being loaded into memory or not.
 
     storage_backend : str
-        Which storage backend to use (only pytables available).
-
-    kernel: str
-        Which CUDA kernel to use (raw or element_wise).
-
-    Attributes
-    ----------
-    fps : Numpy array
-        Fingerprints.
-
-    popcnt_bins : list
-        List with the popcount bins ranges.
-
-    fp_type : str
-        Fingerprint type used to create the fingerprints.
-
-    fp_params : dict
-        Parameters used to create the fingerprints.
-
-    rdkit_ver : dict
-        RDKit version used to create the fingerprints.
+        Which storage backend to use.
     """
 
     raw_kernel = r"""
@@ -75,24 +54,10 @@ class FPSim2CudaEngine(BaseEngine):
         }}
     """
 
-    ew_kernel = r"""
-        int comm_sum = 0;
-        for(int j = 1; j < in_width - 1; ++j){
-            int pos = i * in_width + j;
-            comm_sum += __popcll(db[pos] & query[j]);
-        }
-        float coeff = 0.0;
-        coeff = query[in_width - 1] + db[i * in_width + in_width - 1] - comm_sum;
-        if (coeff != 0.0)
-            coeff = comm_sum / coeff;
-        out[i] = coeff >= threshold ? coeff : 0.0;
-    """
-
     def __init__(
         self,
         fp_filename: str = "",
         storage_backend: str = "pytables",
-        kernel: str = "raw",
         conn_url: str = "",
         table_name: str = "",
         pg_schema: str = "",
@@ -106,66 +71,15 @@ class FPSim2CudaEngine(BaseEngine):
             table_name=table_name,
             pg_schema=pg_schema,
         )
-        self.kernel = kernel
-        if kernel == "raw":
-            # copy all the stuff to the GPU
-            self.cuda_db = cp.asarray(self.fps[:, 1:-1])
-            self.cuda_ids = cp.asarray(self.fps[:, 0])
-            self.cuda_popcnts = cp.asarray(self.fps[:, -1])
-            self.cupy_kernel = cp.RawKernel(
-                self.raw_kernel.format(block=self.cuda_db.shape[1]),
-                name="taniRAW",
-                options=("-std=c++14",),
-            )
-
-        elif self.kernel == "element_wise":
-            # copy the database to the GPU
-            self.cuda_db = cp.asarray(self.fps)
-            self.cupy_kernel = cp.ElementwiseKernel(
-                in_params="raw T db, raw U query, raw T in_width, raw T threshold",
-                out_params="raw V out",
-                operation=self.ew_kernel,
-                name="taniEW",
-                options=("-std=c++14",),
-                reduce_dims=False,
-            )
-
-        else:
-            raise Exception("only supports 'raw' and 'element_wise' kernels")
-
-    def _element_wise_search(self, np_query, threshold):
-        # get the range of the molecule subset to screen
-        fp_range = get_bounds_range(
-            np_query, threshold, 0, 0, self.popcnt_bins, "tanimoto"
+        # copy all the stuff to the GPU
+        self.cuda_db = cp.asarray(self.fps[:, 1:-1])
+        self.cuda_ids = cp.asarray(self.fps[:, 0])
+        self.cuda_db_popcnts = cp.asarray(self.fps[:, -1])
+        self.cupy_kernel = cp.RawKernel(
+            self.raw_kernel.format(block=self.cuda_db.shape[1]),
+            name="taniRAW",
+            options=("-std=c++14",),
         )
-        if fp_range:
-            # copy query and threshold to GPU
-            query = cp.asarray(np_query)
-
-            # get the subset of molecule ids
-            subset_size = int(fp_range[1] - fp_range[0])
-            ids = self.cuda_db[:, 0][slice(*fp_range)]
-
-            # init sims result array
-            sims = cp.zeros(subset_size, dtype="f4")
-
-            self.cupy_kernel(
-                self.cuda_db[slice(*fp_range)],
-                query,
-                self.cuda_db.shape[1],
-                threshold,
-                sims,
-                size=subset_size,
-            )
-
-            mask = sims.nonzero()[0]
-            np_sim = cp.asnumpy(sims[mask])
-            np_ids = cp.asnumpy(ids[mask])
-        else:
-            np_sim = np.ndarray(0)
-            np_ids = np.ndarray(0)
-
-        return np_ids, np_sim
 
     def _raw_kernel_search(self, np_query, threshold):
         # get the range of the molecule subset to screen
@@ -173,49 +87,36 @@ class FPSim2CudaEngine(BaseEngine):
             np_query, threshold, 0, 0, self.popcnt_bins, "tanimoto"
         )
 
-        if fp_range:
-            # copy query and threshold to GPU
-            cuda_threshold = cp.asarray(threshold, dtype="f4")
-            query = cp.asarray(np_query[1:-1])
-            popcount = cp.asarray(np_query[-1])
+        if not fp_range:
+            return np.ndarray(0), np.ndarray(0)
 
-            # get the subset of molecule ids
-            subset_size = int(fp_range[1] - fp_range[0])
-            ids = self.cuda_ids[slice(*fp_range)]
+        cuda_threshold = cp.asarray(threshold, dtype=cp.float32)
+        cuda_query = cp.asarray(np_query[1:-1], dtype=cp.uint64)
+        cuda_query_popcount = cp.asarray(np_query[-1], dtype=cp.uint64)
 
-            # init sims result array
-            sims = cp.zeros(subset_size, dtype=cp.float32)
+        slice_range = slice(*fp_range)
+        grid_size = int(fp_range[1] - fp_range[0])
+        sims = cp.zeros(grid_size, dtype=cp.float32)
+        block_size = self.cuda_db.shape[1]  # number of uint64 elements per fingerprint
 
-            # run in the search, it compiles the kernel only the first time it runs
-            # grid, block and arguments
-            self.cupy_kernel(
-                (subset_size,),
-                (self.cuda_db.shape[1],),
-                (
-                    query,
-                    popcount,
-                    self.cuda_db[slice(*fp_range)],
-                    self.cuda_popcnts[slice(*fp_range)],
-                    cuda_threshold,
-                    sims,
-                ),
-            )
-
-            # get all non 0 values and ids
-            mask = sims.nonzero()[0]
-            np_sim = cp.asnumpy(sims[mask])
-            np_ids = cp.asnumpy(ids[mask])
-        else:
-            np_sim = np.ndarray(0)
-            np_ids = np.ndarray(0)
-
-        return np_ids, np_sim
+        # run the kernel
+        self.cupy_kernel(
+            (grid_size,),
+            (block_size,),
+            (
+                cuda_query,
+                cuda_query_popcount,
+                self.cuda_db[slice_range],
+                self.cuda_db_popcnts[slice_range],
+                cuda_threshold,
+                sims,
+            ),
+        )
+        mask = cp.where(sims > 0)[0]
+        return cp.asnumpy(self.cuda_ids[slice_range][mask]), cp.asnumpy(sims[mask])
 
     def similarity(
-        self,
-        query_string: str,
-        threshold: str,
-        full_sanitization: bool = True,
+        self, query_string: str, threshold: str, full_sanitization: bool = True
     ) -> np.ndarray:
         """Runs a CUDA Tanimoto search.
 
@@ -233,22 +134,14 @@ class FPSim2CudaEngine(BaseEngine):
             Similarity results.
         """
         np_query = self.load_query(query_string, full_sanitization=full_sanitization)
-
-        if self.kernel == "raw":
-            ids, sims = self._raw_kernel_search(np_query, threshold)
-        elif self.kernel == "element_wise":
-            ids, sims = self._element_wise_search(np_query, threshold)
-        else:
-            raise Exception("only supports 'raw' and 'element_wise' kernels")
+        ids, sims = self._raw_kernel_search(np_query, threshold)
 
         # create results numpy array
         results = np.empty(
             len(ids),
-            dtype=np.dtype([("idx", "u4"), ("mol_id", "u4"), ("coeff", "f4")]),
+            dtype=np.dtype([("mol_id", "u4"), ("coeff", "f4")]),
         )
-        results["idx"] = np.zeros(ids.shape[0])  # need to get rid of this
         results["mol_id"] = ids
         results["coeff"] = sims
-        SortResults(results)
-
-        return results[["mol_id", "coeff"]]
+        results[::-1].sort(order="coeff")
+        return results
